acee5d2fc327f4d78f2579513bb4bab75bf03e80
cesaroer <cesarvarcard@gmail.com>
2021-02-10 19:06:15 -0600

    Added scroll view and firts steps for the complex view

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:disenios/src/pages/basico_page.dart';
+import 'package:disenios/src/pages/botones_page.dart';
+import 'package:disenios/src/pages/scroll_page.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -9,9 +11,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Diseños',
-      initialRoute: 'basico',
+      initialRoute: 'botones',
       routes: {
         'basico': (BuildContext context) => BasicoPage(),
+        'scroll': (BuildContext context) => ScrollPage(),
+        'botones': (BuildContext context) => BotonesPage(),
       },
     );
   }
